ad4851c715859f421c1582fc64da97781c7b67b9
RuiFerreira05 <ruimf.05@gmail.com>
2026-03-14 18:42:07 +0000

    feat: enhance logging and connection management in Connection class

--- src/main/java/iecd/a51597/server/Connection.java
+++ src/main/java/iecd/a51597/server/Connection.java
@@ -1,16 +1,35 @@
 package iecd.a51597.server;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.Socket;
 
 public class Connection implements Runnable {
 
     private final Socket clientSocket;
     private final Server server;
+    private final Logger logger = LogManager.getLogger();
+    private InputStream  inputStream;
+    private OutputStream outputStream;
 
     public Connection(Socket client, Server server) {
         this.clientSocket = client;
         this.server = server;
+        initStreams();
+    }
+
+    private void initStreams() {
+        try {
+            this.inputStream = this.clientSocket.getInputStream();
+            this.outputStream = this.clientSocket.getOutputStream();
+        } catch (IOException e) {
+            logger.error("Error initializing streams for connection: {}", e.getMessage());
+            closeConnection();
+        }
     }
 
     @Override
@@ -18,13 +37,21 @@ public class Connection implements Runnable {
         try {
             // TODO: protocol logic
         } finally {
-            try { clientSocket.close(); } catch (IOException ignored) {
-                server.removeConnection(this);
-            }
+            closeConnection();
         }
     }
 
     public Socket getClientSocket() {
         return clientSocket;
+    }
+
+    public void closeConnection() {
+        if (clientSocket.isClosed()) return;
+        server.removeConnection(this);
+        try {
+            clientSocket.close();
+        } catch (IOException e) {
+            logger.error("Error closing connection: {}", e.getMessage());
+        }
     }
 }
