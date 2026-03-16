9597489cc621baacbaaee39342fcc55dc48e0bd0
RuiFerreira05 <ruimf.05@gmail.com>
2026-03-16 23:24:26 +0000

    feat: add new error codes for unexpected message types and actions in ErrorCodeType

--- src/main/java/iecd/a51597/server/MessageHandler.java
+++ src/main/java/iecd/a51597/server/MessageHandler.java
@@ -5,6 +5,8 @@ import iecd.a51597.server.protocol.builders.MessageBuilder;
 import iecd.a51597.server.protocol.exceptions.CommException;
 import iecd.a51597.server.protocol.exceptions.MalformedMessageException;
 import iecd.a51597.server.protocol.parsers.CommParser;
+import iecd.a51597.server.protocol.types.ErrorCodeType;
+import iecd.a51597.server.protocol.types.MessageType;
 
 import java.io.ByteArrayInputStream;
 
@@ -25,13 +27,73 @@ public class MessageHandler {
             Message message = commParser.parseMessage(new ByteArrayInputStream(frameBytes));
             dispatch(message, connection);
         } catch (MalformedMessageException e) {
-            // TODO
+            connection.sendMessage(messageBuilder.errorNoId(
+                    ErrorCodeType.MALFORMED_REQUEST,
+                    "The message does not conform to protocol"
+            ));
         } catch (CommException e) {
-            // TODO
+            connection.sendMessage(messageBuilder.errorNoId(
+                    ErrorCodeType.INTERNAL_ERROR,
+                    "An internal error occurred while processing the message"
+            ));
         }
     }
 
     private void dispatch(Message message, Connection connection) {
-        // TODO
+        if (message.messageType() != MessageType.REQUEST) {
+            connection.sendMessage(messageBuilder.error(
+                    message.messageId(),
+                    message.actionType(),
+                    ErrorCodeType.UNEXPECTED_MESSAGE_TYPE,
+                    "Server only accepts REQUEST messages"
+            ));
+            return;
+        }
+
+        switch (message.actionType()) {
+            case REGISTER:
+                // TODO
+                break;
+            case LOGIN:
+                // TODO
+                break;
+            case LOGOUT:
+                // TODO
+                break;
+            case UPDATE_PROFILE:
+                // TODO
+                break;
+            case SEARCH_USERS:
+                // TODO
+                break;
+            case GAME_INVITE:
+                // TODO
+                break;
+            case GAME_INVITE_RESPONSE:
+                // TODO
+                break;
+            case GAME_MOVE:
+                // TODO
+                break;
+            case GAME_OVER:
+                // TODO
+                break;
+            case UNKNOWN:
+                connection.sendMessage(messageBuilder.error(
+                        message.messageId(),
+                        message.actionType(),
+                        ErrorCodeType.UNEXPECTED_MESSAGE_ACTION,
+                        "Action not expected by server"
+                ));
+                break;
+            default:
+                connection.sendMessage(messageBuilder.error(
+                        message.messageId(),
+                        message.actionType(),
+                        ErrorCodeType.UNKNOWN_ACTION,
+                        "Unknown action type"
+                ));
+                break;
+        }
     }
 }
--- src/main/java/iecd/a51597/server/protocol/types/ErrorCodeType.java
+++ src/main/java/iecd/a51597/server/protocol/types/ErrorCodeType.java
@@ -10,7 +10,9 @@ public enum ErrorCodeType {
     INVALID_PASSWORD,
     UNKNOWN_ACTION,
     INTERNAL_ERROR,
-    MALFORMED_REQUEST;
+    MALFORMED_REQUEST,
+    UNEXPECTED_MESSAGE_TYPE,
+    UNEXPECTED_MESSAGE_ACTION;
 
     public static ErrorCodeType fromString(String string) {
         String normalized = string.replace("-", "_").toUpperCase();
