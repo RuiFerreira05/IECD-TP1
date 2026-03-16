661e065a00fc92609587a8c3e4fe6ec371a2a572
RuiFerreira05 <ruimf.05@gmail.com>
2026-03-16 22:20:29 +0000

    feat: add sealed interface MessageBody with various message types in Message class

--- src/main/java/iecd/a51597/server/protocol/Message.java
+++ src/main/java/iecd/a51597/server/protocol/Message.java
@@ -3,6 +3,7 @@ package iecd.a51597.server.protocol;
 import iecd.a51597.server.protocol.types.ActionType;
 import iecd.a51597.server.protocol.types.BodyKey;
 import iecd.a51597.server.protocol.types.MessageType;
+import org.w3c.dom.Element;
 
 import java.util.Map;
 import java.util.UUID;
@@ -13,5 +14,34 @@ public record Message(
         String version,
         ActionType actionType,
         UUID sessionToken,
-        Map<BodyKey, String> body) {
+        MessageBody body) {
+
+    private sealed interface MessageBody {
+        record Register(String username, String password) implements MessageBody {
+        }
+
+        record Login(String username, String password) implements MessageBody {
+        }
+
+        record Logout() implements MessageBody {
+        }
+
+        record UpdateProfile(String username, String password, String photo) implements MessageBody {
+        }
+
+        record SearchUsers(String query) implements MessageBody {
+        }
+
+        record GameInvite(UUID targetUserId) implements MessageBody {
+        }
+
+        record GameInviteResponse(UUID gameId, boolean accept) implements MessageBody {
+        }
+
+        record GameMove(UUID gameId, Element move) implements MessageBody {
+        }
+
+        record Unknown() implements MessageBody {
+        }
+    }
 }
