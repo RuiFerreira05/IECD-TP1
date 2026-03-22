20e028c136e32d0a3af89ba3379c2d5f4e4a4312
RuiFerreira05 <ruimf.05@gmail.com>
2026-03-22 15:40:51 +0000

    feat: add setter methods for username, passwordHash, and photo fields in User class

--- src/main/java/iecd/a51597/server/store/User.java
+++ src/main/java/iecd/a51597/server/store/User.java
@@ -7,7 +7,7 @@ public class User {
     private UUID userId;
     private String username;
     private String passwordHash;
-    private String photo;
+    private String photo; // can be null
 
     public User(UUID userId, String username, String passwordHash, String photo) {
         this.userId = userId;
@@ -20,6 +20,10 @@ public class User {
         return username;
     }
 
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
     public UUID getUserId() {
         return userId;
     }
@@ -28,7 +32,15 @@ public class User {
         return passwordHash;
     }
 
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+
     public String getPhoto() {
         return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
     }
 }
