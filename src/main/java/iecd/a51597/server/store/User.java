65844bbb53e05ce0f555e048ce9ce4e49d16f8dd
RuiFerreira05 <ruimf.05@gmail.com>
2026-03-22 15:30:53 +0000

    feat: add passwordHash field and constructor to User class for enhanced user data management

--- src/main/java/iecd/a51597/server/store/User.java
+++ src/main/java/iecd/a51597/server/store/User.java
@@ -3,11 +3,18 @@ package iecd.a51597.server.store;
 import java.util.UUID;
 
 public class User {
-    //TODO
 
     private UUID userId;
     private String username;
+    private String passwordHash;
     private String photo;
+
+    public User(UUID userId, String username, String passwordHash, String photo) {
+        this.userId = userId;
+        this.username = username;
+        this.passwordHash = passwordHash;
+        this.photo = photo;
+    }
 
     public String getUsername() {
         return username;
@@ -15,6 +22,10 @@ public class User {
 
     public UUID getUserId() {
         return userId;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
     }
 
     public String getPhoto() {
